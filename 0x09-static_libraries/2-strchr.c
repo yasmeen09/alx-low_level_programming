b797f36f20a4f5fb188c156149e187f4af35852c
Ahmed <ahmedessam121026@icloud.com>
2023-09-27 02:43:00 +0300

    khg

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Alwyas 0 (Success)
+*/
+
+char *_strchr(char *s, char c)
+{
+	int m = 0;
+
+	for (; s[m] >= '\0'; m++)
+	{
+		if (s[m] == c)
+			return (&s[m]);
+	}
+	return (0);
+}
